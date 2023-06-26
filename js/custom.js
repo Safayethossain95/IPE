750c66cb3d4d87429c8a03d0b2c948684c714cb2
Safayethossain95 <safayet.hossain95@gmail.com>
2023-06-26 14:08:24 +0600

    second

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$('.topslider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow:'<i class="fa-solid fa-chevron-right next"></i>',
+    prevArrow:'<i class="fa-solid fa-chevron-left prev"></i>',
+  });
